15c58a8c2e6bc124140436d9dac343a76d7d2079
Sameer Khavanekar <sameer@cyberdust.com>
2018-01-22 18:29:53 -0800

    Initial commit

---
+++ EtherSExampleSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EtherSExampleSwift
+//
+//  Created by Sameer Khavanekar on 1/22/18.
+//  Copyright © 2018 Radical App LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
